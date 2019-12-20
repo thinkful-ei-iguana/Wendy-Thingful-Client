fa720aad89c7375db4f185e513de8a39f5119488
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2019-12-20 13:10:45 -0800

    updated login form to call login endpoint

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -12,7 +12,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
-        authorization: `basic ${TokenService.getAuthToken()}`
+        authorization: `bearer ${TokenService.getAuthToken()}`
       }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
@@ -21,7 +21,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
-        authorization: `basic ${TokenService.getAuthToken()}`
+        authorization: `bearer ${TokenService.getAuthToken()}`
       }
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
@@ -32,7 +32,7 @@ const ThingApiService = {
       method: "POST",
       headers: {
         "content-type": "application/json",
-        authorization: `basic ${TokenService.getAuthToken()}`
+        authorization: `bearer ${TokenService.getAuthToken()}`
       },
       body: JSON.stringify({
         thing_id: thingId,
